6f6d62bcafc123c84575809d6e2d27dee09199e0
MrGuwee <MrGuwee@MRGUWEE-D>
2024-01-05 09:00:57 +0200

    Add README.md.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController: BaseApiController
+    {
+        private readonly DataContext Ctx;
+        public ActivitiesController(DataContext context)
+        {
+            this.Ctx = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await this.Ctx.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await this.Ctx.Activities.FindAsync(id);
+        }
+    }
+}
